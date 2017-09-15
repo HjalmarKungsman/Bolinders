eac2d31a9d1fd56f1030f710c526233ccd1a7228
RarexWU16 <info@danielboman.se>
2017-09-15 15:14:03 +0200

    Initial commit with datamodels

---
+++ Bolinders.Core/Models/Facility.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bolinders.Core.Models
+{
+    public class Facility
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ Bolinders.Core/Models/Vehicle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bolinders.Core.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public Make Make { get; set; }
+        public string Model { get; set; }
+        public string ModelDescription { get; set; }
+        public int Year { get; set; }
+        public int Milage { get; set; }
+        public Decimal Price { get; set; }
+        public string ChassiType { get; set; }
+        public string Colour { get; set; }
+        public string Gearbox { get; set; }
+        public string FuelType { get; set; }
+        public int Horsepowers { get; set; }
+        public bool Used { get; set; }
+        public Facility Facility { get; set; }
+        public List<string> ImageUrl { get; set; }
+        public bool Leasable { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+        public Dictionary<string,string> Equipment { get; set; }
+    }
+}
