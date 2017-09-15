102515e7d935a1b70d84ffddabb2f823b67edb0f
RarexWU16 <info@danielboman.se>
2017-09-15 15:33:42 +0200

    New initial commit with data models

---
+++ Bolinders.Core/Models/Make.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bolinders.Core.Models
+{
+    public class Make
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string FileName { get; set; }
+    }
+}
