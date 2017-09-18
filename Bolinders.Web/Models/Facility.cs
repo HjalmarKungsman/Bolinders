f845f915f19eb44abbd74e9900a48d0f71fd4e81
Timotej Ghost <timotej.ghost@gmail.com>
2017-09-18 10:19:55 +0200

    Initial

--- Bolinders.Web/Models/Facility.cs
+++ Bolinders.Web/Models/Facility.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Bolinders.Web.Models
 {
     public class Facility
     {
+        [Key]
         public string Id { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
--- Bolinders.Web/Models/Vehicle.cs
+++ Bolinders.Web/Models/Vehicle.cs
@@ -13,22 +13,22 @@ namespace Bolinders.Web.Models
         public string Model { get; set; }
         public string ModelDescription { get; set; }
         public int Year { get; set; }
-        public int Milage { get; set; }
+        public int Mileage { get; set; }
         public Decimal Price { get; set; }
-        public string ChassiType { get; set; }
+        //Sedan, Kombi, Halvkombi, Sportkubé, SUV, Cab, Minibuss, Övrigt, Transportbil
+        public string BodyType { get; set; }
         public string Colour { get; set; }
         public string Gearbox { get; set; }
+        //Bensin, Bensin/el, Bensin/etanol, Bensin/gas, Diesel, Diesel/el, El, Gas
         public string FuelType { get; set; }
         public int Horsepowers { get; set; }
         public bool Used { get; set; }
         public string FacilityId { get; set; }
         public virtual Facility Facility { get; set; }
-        // ImageUrl should be a List<string>
-        public string ImageUrl { get; set; }
+        public ICollection<Image> Images { get; set; }
         public bool Leasable { get; set; }
         public DateTime Created { get; set; }
         public DateTime Updated { get; set; }
-        //public Dictionary<string,string> Equipment { get; set; }
-        public string Equipment { get; set; }
+        public ICollection<Equipment> Equipment { get; set; }
     }
 }
