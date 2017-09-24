a4d2fdf036aad25cd79b9c42693cd4e859b67b3e
RarexWU16 <info@danielboman.se>
2017-09-24 23:14:02 +0200

    Email and contactform

--- Bolinders.Core/Services/EmailSenderService.cs
+++ Bolinders.Core/Services/EmailSenderService.cs
@@ -1,45 +1,75 @@
 ﻿using Bolinders.Core.Helpers;
 using Bolinders.Core.Models;
+using Microsoft.AspNetCore.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Mail;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Bolinders.Core.Services
 {
     public class EmailSenderService
     {
-        public static void SendEmailToFacility(string sender, Facility reciever, string subject, string message)
+        
+        public static SmtpStatusCode SendEmailToFacility(string senderName, string senderEmail, string reciever, string subject, string message, string phoneNumber)
         {
-            SmtpClient client = new SmtpClient("mysmtpserver")
+            SmtpClient client = new SmtpClient("mailcluster.loopia.se")
             {
+                Port = 587,
                 UseDefaultCredentials = false,
-                Credentials = new NetworkCredential("username", "password")
+                Credentials = new NetworkCredential("bolindersbil@byteshift.se", "MSD8921%ewf13Xf")
             };
 
-            MailMessage mailMessage = new MailMessage();
-            mailMessage.From = new MailAddress(sender);
-            mailMessage.To.Add(reciever.Email);
-            mailMessage.Body = message;
-            mailMessage.Subject = "Skickat från kontaktformulär - " + subject;
-            client.Send(mailMessage);
+            try
+            {
+
+                message = message + "<br />Telefon: " + phoneNumber + "<br />Namn: " + senderName;
+
+                MailMessage mailMessage = new MailMessage();
+                mailMessage.From = new MailAddress(senderEmail);
+                mailMessage.To.Add(reciever);
+                mailMessage.Body = message;
+                mailMessage.Subject = "Skickat från kontaktformulär: " + subject;
+                client.Send(mailMessage);
+
+                return SmtpStatusCode.Ok;
+            }
+            catch (Exception)
+            {
+                return SmtpStatusCode.GeneralFailure;
+            }          
         }
 
-        public static void SendEmailWithSharedVehicle(string senderName, string senderEmail, string reciever, Vehicle vehicle)
+        public static SmtpStatusCode SendEmailWithSharedVehicle(string senderName, string senderEmail, string reciever, Vehicle vehicle, string baseUrl)
         {
-            SmtpClient client = new SmtpClient("mysmtpserver")
+            SmtpClient client = new SmtpClient("mailcluster.loopia.se")
             {
                 UseDefaultCredentials = false,
-                Credentials = new NetworkCredential("username", "password")
+                Credentials = new NetworkCredential("bolindersbil@byteshift.se", "MSD8921%ewf13Xf")
             };
+            try
+            {
 
-            MailMessage mailMessage = new MailMessage();
-            mailMessage.From = new MailAddress(senderEmail);
-            mailMessage.To.Add(reciever);
-            mailMessage.Body = EmailBuilders.ShareVehicleMessageBuilder(vehicle);
-            mailMessage.Subject = String.Format("{0} vill tipsa dig om en {1} {2} {3}",senderName, vehicle.Make, vehicle.Model, vehicle.ModelDescription);
-            client.Send(mailMessage);
+                var message = string.Format("<h1>{0} {1} {2}</h1>" +
+                   "<p>{5} vill att du ska kolla på:</p>" +
+                "<a href='{3}{4}'>{0} {1} {2}</a>" +
+                "<p>Bilen finns hos Bolinders Bil AB</p>", vehicle.Make, vehicle.Model, vehicle.ModelDescription, baseUrl, vehicle.Id, senderName);
+
+                MailMessage mailMessage = new MailMessage();
+                mailMessage.From = new MailAddress(senderEmail);
+                mailMessage.To.Add(reciever);
+                mailMessage.Body = message;
+                mailMessage.Subject = String.Format("{0} vill tipsa dig om en {1} {2} {3}", senderName, vehicle.Make, vehicle.Model, vehicle.ModelDescription);
+                client.Send(mailMessage);
+
+                return SmtpStatusCode.Ok;
+            }
+            catch (Exception)
+            {
+                return SmtpStatusCode.GeneralFailure;
+            }      
         }
     }
 }
