a4bed5bddff777b9c78a0cd81f1d4de949a894d5
Hjalmar Kungsman <hjalmar.kungsman@gmail.com>
2017-09-24 21:10:43 +0200

    Fix: Added comments to TagHelper

--- Bolinders.Web/Infrastructure/PagingTagHelper.cs
+++ Bolinders.Web/Infrastructure/PagingTagHelper.cs
@@ -33,6 +33,7 @@ namespace Bolinders.Web.Infrastructure
         public string PageClassSelected { get; set; }
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
+            // Generate a querystring if search or filter is active.
             string queryString = "";
             bool first = true;
             List<string> keys = new List<string>();
@@ -56,31 +57,7 @@ namespace Bolinders.Web.Infrastructure
                 queryString = "?" + queryString;
             }
 
-            var test = keys;
-
-
-            //int j = 1;
-            //foreach (var prop in PageModel.SeachModel.GetType().GetProperties())
-            //{
-            //    if (j == 1) { queryString = "&"; }
-            //    if (prop.GetValue(PageModel.SeachModel, null) != null)
-            //    {
-            //        queryString = string.Concat(queryString, prop.GetValue(PageModel.SeachModel, null));
-            //    }
-            //    j++;
-            //}
-            //if (queryString != null)
-            //{
-            //    queryString = string.Concat("?", queryString);
-            //}
-
-            //var antal = j;
-
-
-
-            //var hrefString = "?used=true";
-
-
+            // Builds the <a href=""> tag
             IUrlHelper urlHelper = _helper.GetUrlHelper(ViewContext);
             TagBuilder result = new TagBuilder("div");
             
