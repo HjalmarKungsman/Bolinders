426658b843ecad50bdeda76ae02a8a4de51d4a90
Jesper <R3wE@hotmail.com>
2017-10-05 18:59:00 +0200

    Priskalkyl
    Design behövs

---
+++ Bolinders.Web/wwwroot/js/Details.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    function calculateCost() {
+        const length = parseInt($(".col-lg-6 > .LeaseCalculator form select").val());
+        const price = parseFloat($(".col-lg-6 > .LeaseCalculator form input[type=hidden]").val());
+        const payment = parseFloat($(".col-lg-6 > .LeaseCalculator form input[type=number]").val());
+        const monthlycost = Math.round((price - payment) / length * 12 * 1.045 / 12);
+        const totalcost = monthlycost * length;
+        $("span.monthlyPrice").text(monthlycost + " kr");
+        $("span.totalPrice").text(totalcost + " kr");
+    }
+    $(".col-lg-6 > .LeaseCalculator form").on("submit", function (e) {
+        calculateCost();
+        e.preventDefault();
+    });
+});
