0bc300f873d4d7fbc62c721b35ca089864713a88
Hjalmar Kungsman <hjalmar.kungsman@gmail.com>
2017-10-06 15:07:45 +0200

    COPY: Jesper-E: "Lease calculator (WIP)" commit

--- Bolinders.Web/wwwroot/js/Details.js
+++ Bolinders.Web/wwwroot/js/Details.js
@@ -3,7 +3,7 @@
         const length = parseInt($(".col-lg-6 > .LeaseCalculator form select").val());
         const price = parseFloat($(".col-lg-6 > .LeaseCalculator form input[type=hidden]").val());
         const payment = parseFloat($(".col-lg-6 > .LeaseCalculator form input[type=number]").val());
-        const monthlycost = Math.round((price - payment) / length * 12 * 1.045 / 12);
+        const monthlycost = ((price - payment) / length);
         const totalcost = monthlycost * length;
         $("span.monthlyPrice").text(monthlycost + " kr");
         $("span.totalPrice").text(totalcost + " kr");
