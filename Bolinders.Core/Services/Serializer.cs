001410fe03bbf0bf25a8ac39631ecc5117c13569
Hjalmar Kungsman <hjalmar.kungsman@gmail.com>
2017-10-06 18:19:46 +0200

    Service: XML-serializer/deserializer

---
+++ Bolinders.Core/Services/Serializer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Bolinders.Core.Services
+{
+    class Serializer
+    {
+        public T Deserialize<T>(string input) where T : class
+        {
+            System.Xml.Serialization.XmlSerializer ser = new System.Xml.Serialization.XmlSerializer(typeof(T), new XmlRootAttribute("cars"));
+
+            using (StringReader sr = new StringReader(input))
+            {
+                return (T)ser.Deserialize(sr);
+            }
+        }
+
+        public string Serialize<T>(T ObjectToSerialize)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(ObjectToSerialize.GetType(), new XmlRootAttribute("cars"));
+
+            using (StringWriter textWriter = new StringWriter())
+            {
+                xmlSerializer.Serialize(textWriter, ObjectToSerialize);
+                return textWriter.ToString();
+            }
+        }
+    }
+}
